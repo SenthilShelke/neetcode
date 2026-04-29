9dbf85d552fdc9cedfe1e6c15cd8aa4f00adadc7
SenthilShelke <151959698+SenthilShelke@users.noreply.github.com>
2026-04-29 10:00:49 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cache = {}
+
+        def dp(i, holding):
+            if i == len(prices):
+                return 0
+            if (i, holding) in cache:
+                return cache[(i, holding)]
+            if not holding:
+                buy = -prices[i] + dp(i + 1, True)
+                skip = dp(i + 1, False)
+                ans = max(buy, skip)
+            else:
+                sell = prices[i]
+                keep = dp(i + 1, True)
+                ans = max(sell, keep)
+            cache[(i, holding)] = ans
+            return ans
+
+        return dp(0, False)
+
+        
+
